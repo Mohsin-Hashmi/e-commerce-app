a68862df115a42a5818e6f8cd2a8ff1c7f9cf75b
Mohsin Hashmi <mnadeemhashmi2000@gmail.com>
2025-02-11 11:13:59 +0500

    Developing the signup page

--- e-commerce-frontend/src/components/Signup/Signup.jsx
+++ e-commerce-frontend/src/components/Signup/Signup.jsx
@@ -1,9 +1,25 @@
 
-
+import signupImage from "../../assets/images/signup-bg-image.webp";
+import './Signup.css';
 const Signup = () => {
   return (
     <>
-      <h1>Hello</h1>
+    <section className="signupSec">
+      <div className="signupSecWrapper">
+        <div className="imageContainer">
+          <img className="signupPageImage" src={signupImage} alt="signup image"/>
+        </div>
+        <div className="signupFormWrapper">
+          <h1 className="signupHeading">Sign Up</h1>
+          <form className="signupForm" action="">
+            <input className="inputField emailField" type="email" placeholder="Your email"/>
+            <input className="inputField" type="password" placeholder="Password" />
+            <input className="inputField" type="password" placeholder="Repeat Password" />
+            <button className="signupButton">Sign Up</button>
+          </form>
+        </div>
+      </div>
+    </section>
     </>
   )
 }
