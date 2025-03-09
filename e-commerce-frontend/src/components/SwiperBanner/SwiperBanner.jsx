26dfdc53cef84e779fe9590052ab234947b74eec
Mohsin Hashmi <mnadeemhashmi2000@gmail.com>
2025-03-09 10:25:15 +0500

    Developing the header compoent

--- e-commerce-frontend/src/components/SwiperBanner/SwiperBanner.jsx
+++ e-commerce-frontend/src/components/SwiperBanner/SwiperBanner.jsx
@@ -1,44 +1,86 @@
-import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
-import './SwiperBanner.css';
-import bannerImageOne from '../../assets/images/bannerImage01.webp';
-import bannerImageTwo from '../../assets/images/bannerImage02.webp';
-import bannerImageThree from '../../assets/images/bannerImage03.webp';
-import bannerImageFour from '../../assets/images/bannerImage04.webp';
-import bannerImageFive from '../../assets/images/bannerImage05.webp';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "./SwiperBanner.css";
+import bannerImageOne from "../../assets/images/bannerImage01.webp";
+import bannerImageTwo from "../../assets/images/bannerImage02.webp";
+import bannerImageThree from "../../assets/images/bannerImage03.webp";
+import bannerImageFour from "../../assets/images/bannerImage04.webp";
+import bannerImageFive from "../../assets/images/bannerImage05.webp";
 const SwiperBanner = () => {
-  return (
-    <Swiper
-      // install Swiper modules
-      modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={40}
-      slidesPerView={1}
-      navigation
-      pagination={{ clickable: true }}
-      onSwiper={(swiper) => console.log(swiper)}
-      onSlideChange={() => console.log('slide change')}
-    >
-      <SwiperSlide>
-        <img className='bannerImages' src={bannerImageOne} alt="banner image" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img className='bannerImages' src={bannerImageTwo} alt="banner image" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img className='bannerImages' src={bannerImageThree} alt="banner image" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img className='bannerImages' src={bannerImageFour} alt="banner image" />
-      </SwiperSlide>
-      <SwiperSlide>
-        <img className='bannerImages' src={bannerImageFive} alt="banner image" />
-      </SwiperSlide>
-    
-    </Swiper>
-  )
-}
+  const settings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
 
-export default SwiperBanner
+  return (
+    <div style={{ width: '100%', height: '500px' }}> {/* Fixed container height */}
+      <Slider {...settings} style={{ height: '100%' }}>
+        <div style={{ height: '500px' }}> {/* Fixed slide height */}
+          <img 
+            src={bannerImageOne}
+            alt="Slide 1" 
+            style={{ 
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover', // Prevents distortion
+            }}
+          />
+        </div>
+        <div style={{ height: '500px' }}>
+          <img 
+            src={bannerImageTwo}
+            alt="Slide 2" 
+            style={{ 
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+            }}
+          />
+        </div>
+        <div style={{ height: '500px' }}>
+          <img 
+            src={bannerImageThree}
+            alt="Slide 2" 
+            style={{ 
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+            }}
+          />
+        </div>
+        <div style={{ height: '500px' }}>
+          <img 
+            src={bannerImageFour}
+            alt="Slide 2" 
+            style={{ 
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+            }}
+          />
+        </div>
+        <div style={{ height: '500px' }}>
+          <img 
+            src={bannerImageFive}
+            alt="Slide 2" 
+            style={{ 
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+            }}
+          />
+        </div>
+      </Slider>
+    </div>
+  );
+};
+
+export default SwiperBanner;
