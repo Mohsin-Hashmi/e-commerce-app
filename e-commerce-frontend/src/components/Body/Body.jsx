1db66cb14a54d822cd5f05ca7d8bea11d043b3c5
Mohsin Hashmi <mnadeemhashmi2000@gmail.com>
2025-03-09 11:18:01 +0500

    Developing the body component and banner section is completed

--- e-commerce-frontend/src/components/Body/Body.jsx
+++ e-commerce-frontend/src/components/Body/Body.jsx
@@ -1,15 +1,20 @@
-
-import SwiperBanner from "../SwiperBanner/SwiperBanner"
+import "./Body.css";
 const Body = () => {
   return (
-    <section>
-        <div className="container">
-            <div>
-                <SwiperBanner/>
-            </div>
+    <section className="bannerSec">
+      <div className="container">
+        <div className="bannerSecWrapper">
+          <p className="newArrivalPara">New Arrival</p>
+          <h1 className="collectionHeading">Discover Our New Collection</h1>
+          <p className="collectionPara">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit
+            tellus, luctus nec ullamcorper mattis.
+          </p>
+          <button className="buyNowBtn">BUY NOW</button>
         </div>
+      </div>
     </section>
-  )
-}
+  );
+};
 
-export default Body
+export default Body;
