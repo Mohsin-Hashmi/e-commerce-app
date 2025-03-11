ede42396ad4ae3f7297a29c57a539b3fbec3b727
Mohsin Hashmi <mnadeemhashmi2000@gmail.com>
2025-03-11 21:18:17 +0500

    Developed the footer component

--- e-commerce-frontend/src/components/Footer/Footer.jsx
+++ e-commerce-frontend/src/components/Footer/Footer.jsx
@@ -1,9 +1,74 @@
-
-
+import "./Footer.css";
+import funiroLogo from "../../assets/images/funiroLogo.webp";
+import { Link } from "react-router-dom";
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <>
+      <footer className="footer">
+        <div className="container">
+          <div className="footerWrapper">
+            <div className="footerInfo">
+              <Link to="">
+                <img src={funiroLogo} alt="funiro logo" />
+              </Link>
+              <p className="address">
+                400 University Drive Suite 200 Coral Gables,
+              </p>
+              <p className="location">FL 33134 USA</p>
+            </div>
+            <div className="footerPagesLinks">
+              <div>
+                <h4 className="footerPagesLinksHeading">Links</h4>
+                <ul>
+                  <li>
+                    <Link className="footerPagesLinksOptions">Home</Link>
+                  </li>
+                  <li>
+                    <Link className="footerPagesLinksOptions">Shop</Link>
+                  </li>
+                  <li>
+                    <Link className="footerPagesLinksOptions">About</Link>
+                  </li>
+                  <li>
+                    <Link className="footerPagesLinksOptions">Contact</Link>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <h4 className="footerPagesLinksHeading">Help</h4>
+                <ul>
+                  <li>
+                    <Link className="footerPagesLinksOptions">
+                      Payment Options
+                    </Link>
+                  </li>
+                  <li>
+                    <Link className="footerPagesLinksOptions">Return</Link>
+                  </li>
+                  <li>
+                    <Link className="footerPagesLinksOptions">
+                      Privacy Policies
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+              <div>
+                <h4 className="footerPagesLinksHeading">Newsletter</h4>
 
-export default Footer
+                <input
+                  className="emailInput"
+                  type="email"
+                  placeholder="Enter Your Email Address"
+                  required
+                />
+                <button  className="subscribeBtn">SUBSCRIBE</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
